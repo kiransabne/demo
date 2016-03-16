8fbe93b850503649b7c91bcb4e03cab04c39f6ce
kiran sabne <kiransabne18@gmail.com>
2016-03-16 19:30:11 +0000

    adding images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316075219) do
+ActiveRecord::Schema.define(version: 20160316182404) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160316075219) do
     t.string   "parking"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
 end
