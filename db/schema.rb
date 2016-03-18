367aa76c33ea96fa72b8755203ee958fa42ae2ba
kiran sabne <kiransabne18@gmail.com>
2016-03-18 06:20:13 +0000

    mingled reviews restaurant user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318042047) do
+ActiveRecord::Schema.define(version: 20160318055729) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -31,9 +31,10 @@ ActiveRecord::Schema.define(version: 20160318042047) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "user_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
